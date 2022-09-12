20a7fa82ac6478c6d56c9c618c58149859c6a8a3
EmanRiziq <emanriziq@gmail.com>
2022-09-12 22:54:49 +0300

    edit heroku postgres

--- models/index.js
+++ models/index.js
@@ -3,15 +3,20 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const Posts = require('./post.model')
 
-const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://eman:0000@localhost:5432/eman"
+// const POSTGRES_URL = process.env.DATABASE_URL || "postgresql://eman:0000@localhost:5432/eman"
 
+const POSTGRES_URL = process.env.HEROKU_POSTGRESQL_MAUVE_URL ||  "postgresql://eman:0000@localhost:5432/eman";
 
+const sequelizeOption = {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+}
 
-
-
-
-
-let sequelize = new Sequelize(POSTGRES_URL);
+let sequelize = new Sequelize(POSTGRES_URL, sequelizeOption);
 
 module.exports = {
   db: sequelize,
