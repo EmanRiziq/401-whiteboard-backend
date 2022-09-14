4d63e8c6f7c532b1c8a930ec6e5569a780f46ea2
EmanRiziq <emanriziq@gmail.com>
2022-09-14 21:48:35 +0300

    lab5

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const cors = require('cors');
 const app = express();
 
 const postsRouter = require('./routes/post.route');
+const commentsRouter = require('./routes/comment.route');
+
 const errorHandler404 = require('./error-handlers/404');
 const errorHandler500 = require('./error-handlers/500');
 
 app.use(cors());
 app.use(express.json());
 app.use(postsRouter);
+app.use(commentsRouter)
 
 app.get('/', (req, res) => {
     console.log("home")
